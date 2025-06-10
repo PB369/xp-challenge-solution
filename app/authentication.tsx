c9505ebc8ac3e2390824c78b8528b982bbe7f371
PB369 <pedrohbarros369@gmail.com>
2025-06-09 23:43:49 -0300

    Inputs and credentials validation for authentication have been added

--- app/authentication.tsx
+++ app/authentication.tsx
@@ -1,15 +1,25 @@
-import { Button, Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import '@/global.css'
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function Authentication() {
   const { login } = useAuth();
   const router = useRouter();
 
+  const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [showErrorMessage, setShowErrorMessage] = useState<boolean>(false);
+
   const handleLogin = () => {
-    login();
-    router.replace('/(tabs)');
+    setShowErrorMessage(false)
+    if(username === "admin" && password === "12345"){
+      login();
+      router.replace('/(tabs)');
+    } else {
+      setShowErrorMessage(true);
+    }
   }
 
   return (
@@ -21,7 +31,16 @@ export default function Authentication() {
       }}
     >
       <Text>Authentication</Text>
+      <TextInput onChangeText={setUsername} placeholder="username (admin)"
+        className="border my-2"
+      />
+      <TextInput onChangeText={setPassword} placeholder="password (12345)"
+        className="border mb-2"
+      />
       <Button title="Login" onPress={handleLogin}/>
+      {showErrorMessage && (
+          <Text>Credenciais inválidas! Tente novamente.</Text>
+      )}
     </View>
   );
 }
